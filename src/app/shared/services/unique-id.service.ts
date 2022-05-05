f33d9cba14e429c4c505386812bebf0ebca16f3a
Ivan Villacorta <ivan_mv23@hotmail.com>
2022-05-05 20:39:47 -0300

    first commit

---
+++ src/app/shared/services/unique-id.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class UniqueIdService {
+
+  private numberOfGenerateIds = 0;
+
+  constructor() { }
+
+  public generateUniqueIdWithPrefix(prefix: string): string {
+    if (!prefix) {
+      throw Error('Prefix can not be empty');
+    }
+
+    const uniqueId = this.generateUniqueId();
+    this.numberOfGenerateIds++;
+    return `${prefix}-${uniqueId}`
+  }
+
+  public getNumberOfGenerateUniqueIds(): number {
+    return this.numberOfGenerateIds;
+  }
+
+  private generateUniqueId(): string {
+    return uuidv4();
+  }
+}
