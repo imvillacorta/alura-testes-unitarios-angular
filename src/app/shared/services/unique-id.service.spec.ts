916d3f5fc1d8bebc97f8071e2499a7830448b299
Ivan Villacorta <ivan_mv23@hotmail.com>
2022-05-05 21:15:39 -0300

    Testes unitarios service unique id

--- src/app/shared/services/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id.service.spec.ts
@@ -1,16 +1,10 @@
-import { TestBed } from '@angular/core/testing';
-
 import { UniqueIdService } from './unique-id.service';
 
-describe('UniqueIdService', () => {
-  let service: UniqueIdService;
+describe(UniqueIdService.name, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+    let service = new UniqueIdService();
+    const id = service.generateUniqueIdWithPrefix('app');
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UniqueIdService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(id).toContain('app-');
   });
 });
